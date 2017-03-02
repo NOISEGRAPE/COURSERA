b7752c3a2e51d0c0deb60ecf0716fc916cad1d66
MIGUEL_RUIZ\Miguel <miguenoisegrape@gmail.com>
2017-03-01 19:09:38 -0600

    Semana 5

---
+++ MascotasApp/app/src/main/java/com/noisegrape/mascotasapp/RecyclerViewFragment.java
@@ -0,0 +1,23 @@
+package com.noisegrape.mascotasapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Miguel on 01/03/2017.
+ */
+
+public class RecyclerViewFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_recyclerview,container,false);
+        return v;
+    }
+}
