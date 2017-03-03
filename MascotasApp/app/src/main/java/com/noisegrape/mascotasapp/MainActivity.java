ae7fb997793d58335b49ea8aabd9bf51616a901b
MIGUEL_RUIZ\Miguel <miguenoisegrape@gmail.com>
2017-03-02 21:21:10 -0600

    Semana 5

--- MascotasApp/app/src/main/java/com/noisegrape/mascotasapp/MainActivity.java
+++ MascotasApp/app/src/main/java/com/noisegrape/mascotasapp/MainActivity.java
@@ -1,13 +1,124 @@
 package com.noisegrape.mascotasapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+import com.noisegrape.mascotasapp.adapter.PageAdapter;
+import com.noisegrape.mascotasapp.fragment.PerfilFragment;
+import com.noisegrape.mascotasapp.fragment.RecyclerViewFragment;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        setUpViewPager();
+
+        if (toolbar != null){
+
+            setSupportActionBar(toolbar);
+
+        }
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mFavoritos:
+                Intent intent = new Intent(this, FavoritosActivity.class);
+                startActivity(intent);
+                return true;
+
+
+            case R.id.mContacto:
+                showDialogoContacto();
+                break;
+
+
+            case R.id.mAcercaDe:
+                showDialogoAcercaDe();
+                break;
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+    private void showDialogoContacto() {
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.activity_contacto);
+
+        dialog.getWindow().setBackgroundDrawable(
+                new ColorDrawable(Color.WHITE));
+
+
+        dialog.show();
+    }
+
+    private void showDialogoAcercaDe() {
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.activity_acerca_de);
+
+        dialog.getWindow().setBackgroundDrawable(
+                new ColorDrawable(Color.WHITE));
+
+
+        dialog.show();
+    }
+
+
+
+
+    private ArrayList<Fragment> agregarFragments(){
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(new RecyclerViewFragment());
+        fragments.add(new PerfilFragment());
+        return  fragments;
+    }
+
+    private void setUpViewPager(){
+
+        viewPager.setAdapter(new PageAdapter(getSupportFragmentManager(),agregarFragments()));
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
---
+++ MascotasApp/app/src/main/java/com/noisegrape/mascotasapp/fragment/RecyclerViewFragment.java
@@ -0,0 +1,63 @@
+package com.noisegrape.mascotasapp.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.noisegrape.mascotasapp.Mascota;
+import com.noisegrape.mascotasapp.R;
+import com.noisegrape.mascotasapp.adapter.MascotaAdaptador;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Miguel on 01/03/2017.
+ */
+
+public class RecyclerViewFragment extends Fragment {
+
+    private ArrayList<Mascota> mascotas;
+    private RecyclerView listaMascotas;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_recyclerview,container,false);
+
+
+
+        listaMascotas = (RecyclerView) v.findViewById(R.id.rvMascotas);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        listaMascotas.setLayoutManager(llm);
+
+        inicializarListaMascotas();
+        inicializarAdaptador();
+
+
+        return v;
+    }
+
+
+    public void inicializarListaMascotas(){
+        mascotas = new ArrayList<Mascota>();
+        mascotas.add(new Mascota("Roco", "23", R.drawable.mascota1, R.drawable.hueso_del_perro_50, R.drawable.hueso_del_perro_48));
+        mascotas.add(new Mascota("Shiva", "15", R.drawable.mascota2, R.drawable.hueso_del_perro_50, R.drawable.hueso_del_perro_48));
+        mascotas.add(new Mascota("Aime", "12", R.drawable.mascota3, R.drawable.hueso_del_perro_50, R.drawable.hueso_del_perro_48));
+        mascotas.add(new Mascota("Bolt", "18", R.drawable.mascota4, R.drawable.hueso_del_perro_50, R.drawable.hueso_del_perro_48));
+        mascotas.add(new Mascota("Scooby", "26", R.drawable.mascota5, R.drawable.hueso_del_perro_50, R.drawable.hueso_del_perro_48));
+    }
+
+    public void inicializarAdaptador(){
+        MascotaAdaptador adaptador = new MascotaAdaptador(mascotas, getActivity());
+        listaMascotas.setAdapter(adaptador);
+    }
+
+}
